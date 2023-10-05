b37d7276dccfb9b46bd749dc5a41b1c203801794
Macmini <himanshusinha@himanshus-Mini.bbrouter>
2023-10-06 01:11:15 +0530

    camera functionality

--- src/screens/index.js
+++ src/screens/index.js
@@ -3,4 +3,5 @@ export { default as IdeasScreen } from "../screens/IdeasScreen/IdeaScreen";
 export { default as InviteScreen } from "../screens/InviteScreen/InviteScreen";
 export { default as PaymentScreen } from "../screens/PaymentScreen/PaymentScreen";
 export { default as SetUpScreen } from "../screens/SetUpScreen/SetUpScreen";
+export { default as DetailsScreen } from "../screens/DetailsScreen/DetailsScreen";
 
