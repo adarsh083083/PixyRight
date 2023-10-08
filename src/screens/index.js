565c5f71a46e79ace8eb9565463d741bef0b8a83
adarsh <adarsh083083@gmail.com>
2023-10-09 01:41:29 +0530

    Untrack file: Remove file from Git tracking

--- src/screens/index.js
+++
@@ -1,6 +0,0 @@
-export { default as HomeScreen } from "../screens/HomeScreen/HomeScreen";
-export { default as IdeasScreen } from "../screens/IdeasScreen/IdeaScreen";
-export { default as InviteScreen } from "../screens/InviteScreen/InviteScreen";
-export { default as PaymentScreen } from "../screens/PaymentScreen/PaymentScreen";
-export { default as SetUpScreen } from "../screens/SetUpScreen/SetUpScreen";
-export { default as DetailsScreen } from "../screens/DetailsScreen/DetailsScreen";
